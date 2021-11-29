0d5529673b228751cf179d152c4e11a4e70bb697
Gabriel Urbanin <gabrielut97@hotmail.com>
2021-11-29 15:03:19 -0300

    spring boot admin

--- src/main/java/br/com/hotmart/java/security/SecurityConfigurations.java
+++ src/main/java/br/com/hotmart/java/security/SecurityConfigurations.java
@@ -41,6 +41,7 @@ public class SecurityConfigurations extends WebSecurityConfigurerAdapter {
     protected void configure(HttpSecurity http) throws Exception {
         http.authorizeRequests()
                 .antMatchers(HttpMethod.POST, "/rest/v1/auth").permitAll()
+                .antMatchers(HttpMethod.GET, "/actuator/**").permitAll()
                 .anyRequest().authenticated()
                 .and().csrf().disable()
                 .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)
